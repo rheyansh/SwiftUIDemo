18f2ede34f0ebef53149f05fd8568f909de62d2c
Raj Sharma <raj.sharma@peoplestrong.com>
2022-05-28 20:23:11 +0530

    local search done

---
+++ SwiftUIDemo/SwiftUIDemo/Views/SearchRepo/SearchRepoViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  SearchRepoViewModel.swift
+//  SwiftUIDemo
+//
+//  Created by Raj Sharma on 28/05/22.
+//  Copyright © 2022 raj.sharma. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchRepoViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchRepoViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchRepoViewModel()
+    }
+}
