de2b6ad5706780bf48512962e655bb3f5de40720
Raj Sharma <raj.sharma@peoplestrong.com>
2022-05-29 10:46:27 +0530

    Edit profile screen designed ith image picker, date picker and option picker

---
+++ SwiftUIDemo/SwiftUIDemo/Views/Profile/EditProfileRowView.swift
@@ -0,0 +1,21 @@
+//
+//  EditProfileRowView.swift
+//  SwiftUIDemo
+//
+//  Created by Raj Sharma on 29/05/22.
+//  Copyright © 2022 raj.sharma. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditProfileRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileRowView()
+    }
+}
---
+++ SwiftUIDemo/SwiftUIDemo/Views/Profile/EditProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  EditProfileView.swift
+//  SwiftUIDemo
+//
+//  Created by Raj Sharma on 29/05/22.
+//  Copyright © 2022 raj.sharma. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
